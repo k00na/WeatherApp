709ac6f53fc8410d1b624459eb17ce5afb0ba1c0
k00na <kunaigor44@gmail.com>
2015-08-18 10:29:38 +0200

    Added a ForeCast class + some code for networking which for now returns an errror

--- app/src/main/java/com/example/k00na_/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/k00na_/weatherapp/MainActivity.java
@@ -2,15 +2,48 @@ package com.example.k00na_.weatherapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity.class.yo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String apiKey = "d34476c3792c97247a27bc13224ba5e0";
+        double latitude = 37.8267;
+        double longitude = -122.423;
+        String ourUrl = "https://api.forecast.io/forecast/" + apiKey + "/" + latitude +"," + longitude;
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(ourUrl)
+                .build();
+
+        Call call = client.newCall(request);
+        try {
+            Response response = call.execute();
+            if(response.isSuccessful())
+                Log.v(TAG, "The call was successful!");
+
+        } catch (IOException e) {
+            Log.e(TAG, "Exception caught: " + e);
+        }
+
+
     }
 
     @Override
